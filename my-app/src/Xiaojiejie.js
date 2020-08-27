5d3ae04a5cb3397a6ff41840d58d962a9205ce6c
jingdd-boop <1736929925@qq.com>
2020-08-27 09:37:44 +0800

    react

--- my-app/src/Xiaojiejie.js
+++ my-app/src/Xiaojiejie.js
@@ -1,25 +1,49 @@
-import React,{Component} from 'react'
+import React,{Component, Fragment} from 'react'
+import './style.css' 
+import XiaojiejieItem from './XiaojiejieItem'
 
 class Xiaojiejie extends Component{
   constructor(props){
     super(props)
     this.state={
-      inputValue:'js',
-      list:[]
+      inputValue:'',
+      list:['基础按摩','精度推背']
     }
   }
   render(){
     return(
-      <div>
+     <Fragment>
         <div>
-          <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
-          <button>增加服务</button>
+          <label htmlFor="jspang">增加服务</label>
+          <input id="jspang" className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+          <button onClick={this.addList.bind(this)}>增加服务</button>
         </div>
         <ul>
-          <li>头部按摩</li>
-          <li>精油推背</li>
+        {
+          this.state.list.map((item,index)=>{
+          return (
+            /*
+               <li 
+              key={index+item}
+              onClick={this.deleteItem.bind(this,index)}  
+              dangerouslySetInnerHTML={{__html:item}}
+            
+             </li>
+            */
+          
+         
+          <XiaojiejieItem
+           key={index+item} 
+           content={item}
+           index={index}
+           //list={this.state.list}
+           deleteItem={this.deleteItem.bind(this)}
+          />   
+          )
+          })
+        }
         </ul>
-      </div>
+      </Fragment>
     )
   }
 
@@ -27,6 +51,20 @@ class Xiaojiejie extends Component{
     //console.log(this)
     this.setState({
       inputValue:e.target.value
+    })
+  }
+  addList(){
+    this.setState({
+      list:[...this.state.list,this.state.inputValue],
+      inputValue:''
+    })
+  }
+  deleteItem(index){
+    console.log(index)
+    let list = this.state.list
+    list.splice(index,1)
+    this.setState({
+      list:list
     })
   }
 }
