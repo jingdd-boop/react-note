1195dd118c9b0555086f7937f4d7e00da8c7cb9f
jingdd-boop <1736929925@qq.com>
2020-08-31 14:57:22 +0800

    react

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { CSSTransition } from 'react-transition-group'
+import { CSSTransition } from 'react-transition-group';
+import {actionCreators} from './store';
+
 
 import {
   HeaderWrapper,
@@ -13,69 +15,55 @@ import {
   SearchWrapper
 } from './style.js';
 
-class Header  extends Component {
- 
-  render() { 
-    return ( 
-      <HeaderWrapper>
-        <Logo/>
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载app</NavItem>
-          <NavItem className="right">登录</NavItem>
-          <NavItem className="right">
-          <i className="iconfont icon-yuyan"></i>
-          </NavItem>
-          <SearchWrapper>
-          <CSSTransition
-           timeout={200} 
-           in={this.props.focused}
-           className="slide">
+const Header = (props) => {
+  return ( 
+  <HeaderWrapper>
+  <Logo/>
+  <Nav>
+    <NavItem className="left active">首页</NavItem>
+    <NavItem className="left">下载app</NavItem>
+    <NavItem className="right">登录</NavItem>
+    <NavItem className="right">
+    <i className="iconfont icon-yuyan"></i>
+    </NavItem>
+    <SearchWrapper>
+      <CSSTransition
+      in={props.focused}
+      timeout={200} 
+      classNames="slide">
 
-            <NavSearch
-            className={this.props.focused ? 'focused ': ''}
-            onFocus={this.props.handleInputFocus}
-            onBlur={this.props.handleInputBlur}
-             ></NavSearch>
-          </CSSTransition>
-          <i className={this.props.focused ? 'focused iconfont icon-icon-search': 'iconfont icon-icon-search'}></i>
-          </SearchWrapper>
-         
-        </Nav>
-        <Addition>
-        <Button className="writting"><i className="iconfont icon-bi"></i>写文章</Button>
-          <Button className="reg">注册</Button>
-          
-        </Addition>
-      </HeaderWrapper>
-     );
-  }
-  handleInputFocus(){
+        <NavSearch
+         className={props.focused ? 'focused': ''}
+          onFocus={props.handleInputFocus}
+          onBlur={props.handleInputBlur}
+        >
+        </NavSearch>
+      </CSSTransition>
+    <i className={props.focused ? 'focused iconfont icon-icon-search': 'iconfont icon-icon-search'}></i>
+    </SearchWrapper>
+   
+  </Nav>
+  <Addition>
+  <Button className="writting"><i className="iconfont icon-bi"></i>写文章</Button>
+    <Button className="reg">注册</Button>
     
-  }
-  handleInputBlur(){
-
-  }
+  </Addition>
+</HeaderWrapper>
+);
 }
  
 const mapStateToProps = (state) => {
   return {
-    focused:state.focused
+    focused:state.header.get('focused')
   }
 }
 const mapDispathToProps = (dispatch) => {
   return {
     handleInputFocus(){
-      const action = {
-        type:'search_focus'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchFocus());
     },
     handleInputBlur(){
-      const action = {
-        type:'search_blur'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchBlur());
     }
   }
 }
