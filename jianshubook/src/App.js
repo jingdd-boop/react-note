6d3e6b21f7c9847c1560c9eb7ebce1dc7cb72348
jingdd-boop <1736929925@qq.com>
2020-09-08 11:02:13 +0800

    项目初始化

---
+++ jianshubook/src/App.js
@@ -0,0 +1,8 @@
+import React, {  Component } from 'react';
+import Header from './common/header';
+function App() {
+  return (
+   <Header />
+  )
+}
+export default App;
---
+++ jianshubook/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.js';
+import App from './App';
+ReactDOM.render( 
+   <div><App /></div> ,
+  document.getElementById('root')
+);
+
+//serviceWorker.unregister();
